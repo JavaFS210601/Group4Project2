4548d6cc013b3a84cee383b4a6d6d617fcbc5844
DeLone Jones III <delonej3@yahoo.com>
2021-07-28 18:09:08 -0400

    offer pool dao interface and select statements for offer pool dao

--- PokemonTradingApp/src/main/java/com/revature/daos/OfferPoolDAOInterface.java
+++ PokemonTradingApp/src/main/java/com/revature/daos/OfferPoolDAOInterface.java
@@ -1,5 +1,23 @@
 package com.revature.daos;
 
+import java.util.List;
+
+import com.revature.models.OfferPool;
+import com.revature.models.PokeUsers;
+
 public interface OfferPoolDAOInterface {
+	
+	public List<OfferPool> getAllOffers();
+	
+	public OfferPool getOfferByID(int findID);
+	
+	public List<OfferPool> getOffersByUser(PokeUsers user);
+	
+	public void setOfferStatus(OfferPool offer, int newStatus);
+	
+	public OfferPool newOffer(OfferPool addOffer);
+	
+	public void replyOffer(OfferPool addToOffer, PokeUsers user, int pokemon_id);
+	
 
 }
