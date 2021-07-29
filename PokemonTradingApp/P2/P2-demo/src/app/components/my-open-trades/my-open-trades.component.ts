819d4a69adbed4db866f7c63446db68605dd1ddc
saadman41040 <golamsadman@iut-dhaka.edu>
2021-07-29 14:58:37 -0500

    changed open trade,my open trade,past trade, pending trade pages

--- PokemonTradingApp/P2/P2-demo/src/app/components/my-open-trades/my-open-trades.component.ts
+++ PokemonTradingApp/P2/P2-demo/src/app/components/my-open-trades/my-open-trades.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Pokemon } from 'src/app/models/pokemon';
+import { PokeFetchService } from 'src/app/services/poke-fetch.service';
 
 @Component({
   selector: 'app-my-open-trades',
@@ -6,10 +8,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-open-trades.component.css']
 })
 export class MyOpenTradesComponent implements OnInit {
-
-  constructor() { }
+  
+  hiddenValue:boolean =true;
+   public pokemon:any = null;
+  // holder =this.get
+  offerArray = this.getArray();
+  constructor(private ps:PokeFetchService) { }
 
   ngOnInit(): void {
+  }
+  getArray():any[] {
+     let Array:any=[];
+    let sprite="";
+    let name="";
+    if(true){this.hiddenValue=true;}
+     
+    
+    for(let i=0;i<=6;i=i+1){
+      
+       
+      
+      var secondary="";
+      var offered="";
+       this.ps.getPokemonFromApi(i+1).subscribe(
+
+      (data:Pokemon) => { this.pokemon=data;
+                       
+                        name=this.pokemon.name;
+                        console.log(name);
+                          
+                        
+                        var offer={
+                            id:i+1,
+                            user:"Ash",
+                            pokemon: name,
+                        }
+                        Array[i]=offer;
+
+                        }, 
+      () => {
+        this.pokemon = null;
+        console.log("Something is wrong I can feel it (pokemon retrieval).");
+      }
+      )
+    }
+      console.log(Array);
+      return Array;
+    
   }
 
 }
--- PokemonTradingApp/P2/P2-demo/src/app/components/open-trades/open-trades.component.ts
+++ PokemonTradingApp/P2/P2-demo/src/app/components/open-trades/open-trades.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Pokemon } from 'src/app/models/pokemon';
+
+
+import { PokeFetchService } from 'src/app/services/poke-fetch.service';
 
 @Component({
   selector: 'app-open-trades',
@@ -6,10 +11,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./open-trades.component.css']
 })
 export class OpenTradesComponent implements OnInit {
-
-  constructor() { }
+  public pokemon:any = null;
+  // holder =this.get
+  offerArray = this.getArray();
+  constructor(private ps:PokeFetchService) { 
+  }
 
   ngOnInit(): void {
+
+  }
+
+   getArray():any[]{
+    let Array:any=[];
+    let sprite="";
+    let name="";
+    for(let i=0;i<13;i++){
+       this.ps.getPokemonFromApi(i+1).subscribe(
+
+      (data:Pokemon) => { this.pokemon=data;
+                        sprite= this.pokemon.sprites.front_shiny;
+                        name=this.pokemon.name;
+                        console.log(sprite);
+                        var offer={
+                            id:i+1,
+                            user:i+1,
+                            pokemon: name,
+                            objsprites: sprite
+                        }
+                        Array[i]=offer;
+                        }, 
+      () => {
+        this.pokemon = null;
+        console.log("Something is wrong I can feel it (pokemon retrieval).");
+      }
+      )
+    }
+      return Array;
+    
   }
 
 }
+
+
